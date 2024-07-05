8f699ceb6a08a86d3b046ec63e116fb89562ceba
harry <knight65210@gmail.com>
2024-07-05 20:20:06 +0800

    modify stop_loss format

--- stop_loss_calculator.py
+++ stop_loss_calculator.py
@@ -9,7 +9,7 @@ import os
 from datetime import date, timedelta
 from dotenv import load_dotenv
 
-#加載 .env 文件中的環境變數
+# 加載 .env 文件中的環境變數
 load_dotenv()
 # 建立API物件，simulation=True是代表測試帳號
 api = sj.Shioaji(simulation=True)
@@ -20,12 +20,12 @@ shioaji_apikey = os.getenv('SHIOAJI_APIKEY', None)
 
 # 登入你的key
 # accounts = api.login("YOUR_API_KEY", "YOUR_SECRET_KEY")
-accounts = api.login(shioaji_apikey,shioaji_secret)
+accounts = api.login(shioaji_apikey, shioaji_secret)
 
 
 def calculate_stop_loss(buy_price):
-    stop_loss_five_percent = buy_price*0.95
-    stop_loss_four_percent = buy_price*0.96
+    stop_loss_five_percent = round(buy_price*0.95, 1)
+    stop_loss_four_percent = round(buy_price*0.96, 1)
     return stop_loss_five_percent, stop_loss_four_percent  # 設定停損價格為買進價格的90%
 
 
@@ -61,4 +61,4 @@ def calculate_moving_averages(stock_code, window=60, buy_price=None):
         df['20MA_diff'] = (buy_price - df['20MA']) / buy_price * 100
         df['60MA_diff'] = (buy_price - df['60MA']) / buy_price * 100
 
-    return df[['5MA', '10MA', '20MA', '60MA', '20MA/60MA', '5MA_diff', '10MA_diff','20MA_diff','60MA_diff']].dropna()
+    return df[['5MA', '10MA', '20MA', '60MA', '20MA/60MA', '5MA_diff', '10MA_diff', '20MA_diff', '60MA_diff']].dropna()
